25a5e8299d84074ae4433801e1e1b64eafaf14c3
Eric Desmars <eric.desmars@gmail.com>
2020-08-16 19:30:22 +0800

    Add events to trigger systems

--- src/systemManager.js
+++ src/systemManager.js
@@ -1,72 +1,117 @@
-import {isArray} from './utils/validate';
+import {isArray, isString} from './utils/validate';
 
 export const SystemManager = (storage, verbose) => {
 
-  const _storage = storage;
-  const _registeredSystems = new Map();
+    const _storage = storage;
+    const _registeredSystems = new Map();
+    const _registerEvents = new Set();
 
 
-  const _log = ( ...$msg) => {
-    if(verbose) {
-      console.log('System Manager: ',...$msg)
-    }
-  }
-
-  const _query = (q) => {
-    return _storage.getEntityByComponents(q);
-  }
-
-  const _validate = (system) => {
-    //check that system is a function with the correct prototype
-    if(!typeof(system) === 'function' || !system.hasOwnProperty('query')) {
-      _log('Trying to register a system that is either not a function or does not have a name and query defined');
-      return false;
+    const _log = (...$msg) => {
+        if (verbose) {
+            console.log('System Manager: ', ...$msg)
+        }
     }
 
-    if(!isArray(system.query, 'string')){
-      _log(`System ${system.name} does not have a correct query. A query must be an array of string.`)
-      return false;
+    const _query = (q) => {
+        return _storage.getEntityByComponents(q);
     }
 
-    return true
-  }
+    const _validate = (system) => {
+        //check that system is a function with the correct prototype
+        if (!typeof (system) === 'function' || !system.hasOwnProperty('query')) {
+            _log('Trying to register a system that is either not a function or does not have a name and query defined');
+            const res = system(new Map())
+            return false;
+        }
 
+        if (!isArray(system.query, 'string')) {
+            _log(`System ${system.name} does not have a correct query. A query must be an array of string.`)
+            return false;
+        }
 
-  /**
-   * Execute all registered systems
-   * @returns {Map{Array}} - Return values from each system
-   */
-  const execute = () => {
-    // loop through all systems
-    const returnValues = new Map();
-    for(let [name, system] of _registeredSystems){
-      const entities = _query(system.query);
-      returnValues.set(name,new Map())
-      entities.forEach((e) => {
-        returnValues.get(name).set(e, system(_storage.getEntityComponents(e)))
-      })
+        if (!isArray(system.events, 'string')) {
+            _log(`System ${system.name} does not have a correct events trigger setup. Systems must have an events property that is an empty array or an array of Events name.`)
+            return false;
+        }
+
+        return true
     }
-    return returnValues;
-  }
 
-  /**
-   * Register a new system
-   * @param {function} system - A system to be registered
-   * @returns {boolean} - True if successful else false
-   */
-  const register = (system) => {
-    if(_validate(system)){
-      _log(`Registering ${system.name} System`)
-      _registeredSystems.set(system.name, system);
-      return true;
-    } else {
-      return false;
+
+    /**
+     * Execute all registered systems
+     * @returns {Map{Array}} - Return values from each system
+     */
+    const execute = () => {
+        // loop through all systems
+        const returnValues = new Map();
+        for (let [name, system] of _registeredSystems) {
+            const entities = _query(system.query);
+            returnValues.set(name, new Map())
+            entities.forEach((e) => {
+                returnValues.get(name).set(e, system(_storage.getEntityComponents(e)).execute())
+            })
+        }
+        return returnValues;
     }
-  }
 
-  return {
-    execute,
-    register
-  }
+    /**
+     * Register a new system
+     * @param {function} system - A system to be registered
+     * @returns {boolean} - True if successful else false
+     */
+    const register = (system) => {
+        if (_validate(system)) {
+            _log(`Registering ${system.name} System`)
+            _registeredSystems.set(system.name, system);
+            return true;
+        } else {
+            _log(`System ${system.name} failed validation and was not registered.`);
+            return false;
+        }
+    }
+
+    /**
+     * Register Event
+     * @param {string} eventName - Event Name
+     * @returns {boolean} - True if registered
+     */
+    const registerEvent = (eventName) => {
+        if (isString(eventName)) {
+            _registerEvents.add(eventName);
+            return true;
+        } else {
+            _log(`Event ${eventName} failed validation and was not registered.`);
+            return false;
+        }
+    }
+
+    const triggerEvent = (eventName) => {
+        if(!_registerEvents.has(eventName)) {
+            _log(`Event ${eventName} is not registered.`);
+            return false;
+        }
+
+        // loop through all systems
+        const returnValues = new Map();
+        for (let [name, system] of _registeredSystems) {
+            if(system.events.includes(eventName)){
+                const entities = _query(system.query);
+                returnValues.set(name, new Map())
+                entities.forEach((e) => {
+                    returnValues.get(name).set(e, system(_storage.getEntityComponents(e)).events())
+                })
+            }
+        }
+        return returnValues;
+    }
+
+    return {
+        execute,
+        register,
+        registerEvent,
+        triggerEvent
+    }
 
 }
--- tests/systemManager.test.js
+++ tests/systemManager.test.js
@@ -8,7 +8,7 @@ const mockStorage = {
     addEntityComponent: jest.fn(),
     removeEntityComponent: jest.fn(),
     getEntityByComponents: jest.fn(() => {
-        const e =  new Set();
+        const e = new Set();
         e.add('1234567890')
         return e;
     }),
@@ -19,12 +19,12 @@ const mockStorage = {
 
 
 test('Initialise System Manager return an object of functions', () => {
-    const sm = SystemManager(mockStorage,false)
+    const sm = SystemManager(mockStorage, false)
     expect(sm).toHaveProperty('execute');
     expect(sm).toHaveProperty('register');
 })
 
-describe('Register System',() => {
+describe('Register System', () => {
     let consoleOutput = []
     const mockedLog = (...output) => consoleOutput.push(output)
     beforeEach(() => (console.log = mockedLog))
@@ -32,8 +32,9 @@ describe('Register System',() => {
     afterEach(() => (console.log = originalLog))
 
     it('register a correct system', () => {
-        const sm = SystemManager(mockStorage,true)
-        const mySystem = (c,l) => {}
+        const sm = SystemManager(mockStorage, true)
+        const mySystem = (c, l) => {
+        }
         mySystem.query = ['test']
         const res = sm.register(mySystem);
         expect(res).toBe(true);
@@ -44,13 +45,14 @@ describe('Register System',() => {
     })
 
     it('does not register non valid system', () => {
-        const sm = SystemManager(mockStorage,false)
-        let s = (c,l) => {}
+        const sm = SystemManager(mockStorage, false)
+        let s = (c, l) => {
+        }
         let res = sm.register(s);
         expect(res).toBe(false);
 
         s = {}
-        s.query = [1,2,3]
+        s.query = [1, 2, 3]
         res = sm.register(s);
         expect(res).toBe(false);
 
@@ -62,9 +64,17 @@ describe('Execute Systems', () => {
 
 
     it('execute systems', () => {
-        const sm = SystemManager(mockStorage,false)
-        const s = jest.fn(() => true)
+        const sm = SystemManager(mockStorage, false)
+        const s = jest.fn(() => {
+            const execute = () => true
+            const events = () => true
+            return {
+                execute,
+                events
+            }
+        })
         s.query = ['test']
+        s.events = []
         const res = sm.register(s);
         const execResult = sm.execute()
         expect(mockStorage.getEntityByComponents).toHaveBeenCalledWith(s.query)
@@ -72,5 +82,52 @@ describe('Execute Systems', () => {
         expect(s).toHaveBeenCalledWith(new Set())
         expect(execResult.get('mockConstructor').get('1234567890')).toBe(true)
     });
+
+    it('trigger events', () => {
+        const sm = SystemManager(mockStorage, false)
+        const s = jest.fn(() => {
+            const execute = () => true
+            const events = () => true
+            return {
+                execute,
+                events
+            }
+        })
+        s.query = ['test']
+        s.events = ['click']
+        const res = sm.register(s);
+        sm.registerEvent('click')
+        const execResult = sm.triggerEvent('click')
+        expect(mockStorage.getEntityByComponents).toHaveBeenCalledWith(s.query)
+        expect(mockStorage.getEntityComponents).toHaveBeenCalledWith('1234567890')
+        expect(s).toHaveBeenCalledWith(new Set())
+        expect(execResult.get('mockConstructor').get('1234567890')).toBe(true)
+    })
+
+    it('decline non registered events', () => {
+        const sm = SystemManager(mockStorage, false)
+        const s = jest.fn(() => {
+            const execute = () => true
+            const events = () => true
+            return {
+                execute,
+                events
+            }
+        })
+        s.query = ['test']
+        s.events = []
+        const res = sm.register(s);
+
+        const wrongResult = sm.triggerEvent('unknown')
+        expect(mockStorage.getEntityByComponents).not.toHaveBeenCalled()
+        expect(s).not.toHaveBeenCalledWith(new Set())
+        expect(wrongResult).toBe(false)
+    })
+
+    it('only accept string as eventName', () => {
+        const sm = SystemManager(mockStorage, false)
+        const res = sm.registerEvent(1234)
+        expect(res).toBe(false)
+    })
 
 })
